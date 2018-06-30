25c2d8444317f0d682a84b9bfa683a947d9e4624
Cody <Cody.Turek@hotmail.com>
2018-06-29 18:13:28 -0600

    Added Brorater

---
+++ src/main/java/tech/ugma/brorater/Brorater.java
@@ -0,0 +1,12 @@
+package tech.ugma.brorater;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Brorater extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
