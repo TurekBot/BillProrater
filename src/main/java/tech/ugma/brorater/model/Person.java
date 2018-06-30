55d34836f64a60544e05fbda41501ab63c240a39
Cody <Cody.Turek@hotmail.com>
2018-06-29 22:07:54 -0600

    Changed person fields to properties.

--- src/main/java/tech/ugma/brorater/model/Person.java
+++ src/main/java/tech/ugma/brorater/model/Person.java
@@ -1,46 +1,65 @@
 package tech.ugma.brorater.model;
 
+import javafx.beans.property.*;
+
 import java.time.LocalDate;
 
 public class Person {
-    private String name;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private double balanceDue;
+    private StringProperty name = new SimpleStringProperty();
+    private ObjectProperty<LocalDate> startDate = new SimpleObjectProperty<>();
+    private ObjectProperty<LocalDate> endDate = new SimpleObjectProperty<>();
+    private DoubleProperty balanceDue = new SimpleDoubleProperty();
 
     ///////////////////////////////////////////////////////////////////////////
     // Getters and Setters
     ///////////////////////////////////////////////////////////////////////////
 
+
     public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public LocalDate getStartDate() {
+        return startDate.get();
+    }
+
+    public ObjectProperty<LocalDate> startDateProperty() {
         return startDate;
     }
 
     public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
+        this.startDate.set(startDate);
     }
 
     public LocalDate getEndDate() {
+        return endDate.get();
+    }
+
+    public ObjectProperty<LocalDate> endDateProperty() {
         return endDate;
     }
 
     public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+        this.endDate.set(endDate);
     }
 
     public double getBalanceDue() {
+        return balanceDue.get();
+    }
+
+    public DoubleProperty balanceDueProperty() {
         return balanceDue;
     }
 
     public void setBalanceDue(double balanceDue) {
-        this.balanceDue = balanceDue;
+        this.balanceDue.set(balanceDue);
     }
 }
