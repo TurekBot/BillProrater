51abba70f17094f4fbb7af46f40a2a7b407ca732
Cody <Cody.Turek@hotmail.com>
2018-06-30 00:50:52 -0600

    Added container wrapper class for storing data

---
+++ src/main/java/tech/ugma/brorater/model/Container.java
@@ -0,0 +1,40 @@
+package tech.ugma.brorater.model;
+
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Helper class to wrap a list of Persons and a list of Bills.
+ * This is used for saving the lists to XML.
+ *
+ * @author Marco Jakob
+ * @author Bradley Turek
+ */
+@XmlRootElement(name = "storage")
+public class Container {
+
+    private List<Person> persons;
+    private List<Bill> bills;
+
+    @XmlElement(name = "person")
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+
+    @XmlElement(name = "bill")
+    public List<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(List<Bill> bills) {
+        this.bills = bills;
+    }
+}
