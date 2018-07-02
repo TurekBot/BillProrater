234e4ae862a05e14d2f3ac50946beff9a33ad1ff
Cody <Cody.Turek@hotmail.com>
2018-07-01 23:48:13 -0600

    Added icons!

--- src/main/java/tech/ugma/brorater/Brorater.java
+++ src/main/java/tech/ugma/brorater/Brorater.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Brorater extends Application {
@@ -24,7 +25,27 @@ public class Brorater extends Application {
                 Brorater.class.getResource("/css/brotater-styles.css").toExternalForm());
 
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Bill Prorater");
 
+        // Add icons; JavaFX will decide which one is best, depending on where the icon is showing
+        try {
+            primaryStage.getIcons().addAll(
+                    new Image(this.getClass().getResourceAsStream(
+                            "/images/app-icons/icons8-rent-16.png")),
+                    new Image(this.getClass().getResourceAsStream(
+                            "/images/app-icons/icons8-rent-24.png")),
+                    new Image(this.getClass().getResourceAsStream(
+                            "/images/app-icons/icons8-rent-32.png")),
+                    new Image(this.getClass().getResourceAsStream(
+                            "/images/app-icons/icons8-rent-48.png")),
+                    new Image(this.getClass().getResourceAsStream(
+                            "/images/app-icons/icons8-rent-64.png"))
+            );
+        } catch (NullPointerException npe) {
+            System.err.println("Could not locate application's icons!");
+            npe.printStackTrace();
+//            failGracefully("Could not find some important icons. Something's wrong.");
+        }
 
 
         primaryStage.show();
