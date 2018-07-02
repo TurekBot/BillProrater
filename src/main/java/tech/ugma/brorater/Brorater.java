b647ec25ee18265d7a761297eccc658d37f4f61c
Cody <Cody.Turek@hotmail.com>
2018-07-01 21:04:03 -0600

    * Added some stylesheets. * Moved some things around.

--- src/main/java/tech/ugma/brorater/Brorater.java
+++ src/main/java/tech/ugma/brorater/Brorater.java
@@ -1,6 +1,7 @@
 package tech.ugma.brorater;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,7 +16,16 @@ public class Brorater extends Application {
         Controller controller = fxmlLoader.getController();
         controller.setPrimaryStage(primaryStage);
         Scene scene = new Scene(root);
+
+        // Apply CSS
+        final ObservableList<String> stylesheets = scene.getStylesheets();
+        stylesheets.addAll(Brorater.class.getResource("/css/jfoenix-fonts.css").toExternalForm(),
+                Brorater.class.getResource("/css/colors.css").toExternalForm(),
+                Brorater.class.getResource("/css/brotater-styles.css").toExternalForm());
+
         primaryStage.setScene(scene);
+
+
 
         primaryStage.show();
     }
